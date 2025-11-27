77cd80dc286ca53647e98c7af90b2d9202174036
Gianluca Rossi <gr.gianlucarossi@gmail.com>
2025-11-27 14:12:58 -0500

    feat: read messages.txt 8 bytes at a time and print that data back to stdout in 8 byte chunks

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,24 @@
-fn main() {
-    println!("I hope I get the job!")
+use std::{
+    fs::File,
+    io::{self, BufRead, BufReader},
+};
+
+fn main() -> io::Result<()> {
+    const CAPACITY: usize = 8; // 8 bytes
+    let file = File::open("messages.txt").unwrap();
+    let mut reader = BufReader::with_capacity(CAPACITY, file);
+
+    loop {
+        let buffer = reader.fill_buf()?;
+        let length: usize = buffer.len();
+
+        if length == 0_usize {
+            break;
+        }
+        let text = std::str::from_utf8(buffer).unwrap_or("<invalid utf8>");
+        println!("read: {}", text);
+        reader.consume(length);
+    }
+
+    Ok(())
 }
