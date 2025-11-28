261fbd8878c571ab679f90be7a53405310916fcd
Gianluca Rossi <gr.gianlucarossi@gmail.com>
2025-11-28 00:26:13 -0500

    feat: read line by line

--- src/main.rs
+++ src/main.rs
@@ -1,21 +1,15 @@
 use std::{
     fs::File,
-    io::{self, Read},
+    io::{self, BufRead, BufReader},
 };
 
 fn main() -> io::Result<()> {
-    let mut file = File::open("messages.txt")?;
-    let mut buffer = [0u8; 8];
+    let file = File::open("messages.txt")?;
+    let reader = BufReader::new(file);
 
-    loop {
-        let n = file.read(&mut buffer)?;
-        if n == 0 {
-            break;
-        }
-        // buffer[..n] is necessary for scenario when buffer is less than 8 bytes. this
-        // could happen when reading the last chunk of the file.
-        let text = std::str::from_utf8(&buffer[..n]).unwrap_or("<invalid utf8>");
-        println!("read: {}", text);
+    for line in reader.lines() {
+        let line = line?;
+        println!("read: {}", line);
     }
     Ok(())
 }
