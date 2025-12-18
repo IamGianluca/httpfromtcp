f0e9c44cc44d58f4a45cce3f734cea4b2b4b280a
Gianluca Rossi <gr.gianlucarossi@gmail.com>
2025-12-18 07:06:02 -0500

    feat: modify tcplistener to use request_from_reader to parse the request and print out method, target, and version

--- src/bin/tcplistener.rs
+++ src/bin/tcplistener.rs
@@ -1,48 +1,27 @@
 use std::{
-    io::{self, BufReader, Read},
-    net::{TcpListener, TcpStream},
-    sync::mpsc::{Receiver, channel},
-    thread,
+    io::{self, BufReader},
+    net::TcpListener,
 };
 
-fn get_lines_channel(mut reader: BufReader<TcpStream>) -> Receiver<String> {
-    let (sender, receiver) = channel();
-    thread::spawn(move || {
-        let mut buffer = [0u8; 8];
-        let mut current_line = String::new();
-        loop {
-            match reader.read(&mut buffer) {
-                Ok(0) => break, // EOF
-                Ok(n) => {
-                    let chunk = String::from_utf8_lossy(&buffer[..n]).to_string();
-
-                    if let Some((before_newline, after_newline)) = chunk.split_once("\n") {
-                        current_line.push_str(before_newline);
-                        sender.send(current_line.clone()).unwrap();
-                        current_line = after_newline.to_string();
-                    } else {
-                        current_line.push_str(&chunk);
-                    }
-                }
-                Err(_) => break,
-            }
-        }
-    });
-    receiver
-}
+use httpfromtcp::request::{RequestState, request_from_reader};
 
 fn main() -> io::Result<()> {
     let listener = TcpListener::bind("127.0.0.1:42069")?;
-
     loop {
         let (stream, _addr) = listener.accept()?;
         println!("Connection accepted.");
 
         let reader = BufReader::new(stream);
-        let receiver = get_lines_channel(reader);
+        let request = request_from_reader(reader)?;
 
-        for line in receiver {
-            println!("{}", line);
+        match request.status {
+            RequestState::Done => println!(
+                "Request line:\n- Method: {}\n-Target: {}\n-Version: {}",
+                request.request_line.method,
+                request.request_line.request_target,
+                request.request_line.http_version,
+            ),
+            RequestState::Initialized => continue,
         }
 
         println!("Connection closed.");
